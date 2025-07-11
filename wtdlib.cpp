91585f7628c7f131b434ec150a666d4f1d600c6b
wtdluger <wtdluger@gmail.com>
2025-07-10 19:10:54 -0700

    Read from and Write to csv file

--- wtdlib.cpp
+++ wtdlib.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include<fstream>
+#include<sstream>
 #include <string>
 #include <list>
 #include <vector>
@@ -21,6 +23,57 @@ void printList(list<int>& arr) {
         cout << element << ", ";
     }        
     cout << endl;
+}
+
+/* read a csv file */
+std::vector<std::vector<std::string>> readCSV(std::string file_loc)
+{
+    std::ifstream file(file_loc);
+    if (!file.is_open()) {
+        throw std::runtime_error("Error: Could not open the file.");
+    }
+    std::string line;
+    std::vector<std::vector<std::string>> csv_data; // To store all data
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string field;
+        std::vector<std::string> row_data;
+
+        while (std::getline(ss, field, ',')) {
+            row_data.push_back(field);
+        }
+        csv_data.push_back(row_data);
+    }
+    // close the file when done reading to save resources
+    file.close();
+    return csv_data;
+}
+
+/* write to csv file */
+void writeCSV(
+    std::vector<std::vector<std::string>> csv_data, 
+    std::string outputFileName
+){
+// If it exists, its content will be truncated (cleared) by default.
+    std::ofstream outFile(outputFileName);
+
+    // Check if the file was opened successfully
+    if (outFile.is_open()) {
+        for (const auto& row : csv_data) 
+        {
+            for (const auto& field : row) {
+                outFile << field << "\t";
+            }
+        }
+
+        // Close the file when done writing to release resources
+        outFile.close();
+        std::cout << "Data successfully written to example.txt" << std::endl;
+    } else {
+        // Handle the error if the file could not be opened
+        std::cerr << "Error: Unable to open file." << std::endl;
+    }
 }
 
 /* 
--- wtdlib.h
+++ wtdlib.h
@@ -1,10 +1,21 @@
 #ifndef WTDLIB_H
 #define WTDLIB_H
 
-#include <vector>
+#include <iostream>
+#include<fstream>
+#include<sstream>
+#include <string>
 #include <list>
+#include <vector>
+#include <chrono>
+#include <thread>
 using namespace std;
 
+std::vector<std::vector<std::string>> readCSV(std::string file_loc);
+void writeCSV(
+    std::vector<std::vector<std::string>> csv_data, 
+    std::string outputFileName
+);
 void printVector(vector <int>& arr);
 void printList(list <int>& arr);
 void timer(int duration_ms);
